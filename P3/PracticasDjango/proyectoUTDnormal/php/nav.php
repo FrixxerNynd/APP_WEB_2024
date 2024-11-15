80a2b1a7ebcfc112ca9f09ef2329c1137b5a1314
FrixxerNynd <148486300+FrixxerNynd@users.noreply.github.com>
2024-11-15 08:58:36 -0600

    COMMIT_P3_DJANGO_V1

---
+++ P3/PracticasDjango/proyectoUTDnormal/php/nav.php
@@ -0,0 +1,8 @@
+    <nav>    
+        <ul>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="html/acercade.html">Acerca de</a></li>
+            <li><a href="html/mision.html">Mision</a></li>
+            <li><a href="html/vision.html">Vision</a></li>
+        </ul>
+    </nav>
